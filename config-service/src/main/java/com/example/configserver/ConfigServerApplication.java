67d2fb80857f5524f2078ac63f88586e61a10ca5
Youssef <youssefabdennadher2@gmail.com>
2023-12-25 20:28:29 +0100

    change the config server

--- config-service/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-service/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -1,15 +1,17 @@
-package com.example.configservice;
+package com.example.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServiceApplication {
+@EnableDiscoveryClient
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServiceApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
